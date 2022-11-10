8bc42590b6ff851e0e58a8ce619e93047652144b
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-10 23:37:05 +0530

    promises just as traversy media does

---
+++ promises.js
@@ -0,0 +1,33 @@
+const posts = [
+    { title: 'Post One', body: 'This is Post One' },
+    { title: 'Post Two', body: 'This is Post Two' }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+};
+
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+            if (!error) {
+                resolve();
+            }
+            else {
+                reject('Error: Something went wrong');
+            }
+        }, 2000);
+    });
+};
+
+createPost({ title: 'Post Three', body: 'This is Post Three' })
+    .then(getPosts)
+    .catch(err => console.log(err));
